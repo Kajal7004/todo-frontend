db829a74d4f92c577a8a9e382da30c868ad3a22e
Nishita <nishitaietmca05@gmail.com>
2026-02-25 13:09:48 +0530

    added description and checkbox

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const API_URL = "http://localhost:5000/api";
 
-// 🔐 LOGIN
+//  LOGIN
 async function login() {
   const email = document.getElementById("loginEmail").value;
   const password = document.getElementById("loginPassword").value;
-  console.log("enter email",email);
-  console.log("enter password",password);
-  console.log("checking email",document.getElementById("loginEmail"));
-
+  console.log("enter email", email);
+  console.log("enter password", password);
+  console.log("checking email", document.getElementById("loginEmail"));
 
   try {
     const response = await fetch("http://localhost:5000/api/auth/login", {
@@ -22,43 +21,31 @@ async function login() {
     });
 
     const data = await response.json();
-    console.log("LOGIN RESPONSE 👉", data);
+    console.log("LOGIN RESPONSE ", data);
 
-
-     if (!response.ok) {
+    if (!response.ok) {
       document.getElementById("loginMsg").innerText = data.message;
       return;
     }
 
-    // if (!response.ok) {
-    //   document.getElementById("loginMsg").innerText = data.message;
-    //   return;
-    // }
-    // console.log("data",data);
-
-    // ✅ TOKEN SAVE
+    // TOKEN SAVE
     localStorage.removeItem("token");
     localStorage.setItem("token", data.access_token);
-    // console.log("TOKEN SAVED 👉", data.token);
-    
 
     document.getElementById("loginMsg").innerText =
       "Login successful!";
 
     // Optional redirect
-     window.location.href = "todo.html";
+    window.location.href = "todo.html";
 
   } catch (error) {
-    console.log("LOGIN ERROR 👉", error);
+    console.log("LOGIN ERROR ", error);
     document.getElementById("loginMsg").innerText =
       "Something went wrong";
   }
 }
 
-
-
-
-// 📝 REGISTER
+//  REGISTER
 function register() {
   const name = document.getElementById("regName").value;
   const email = document.getElementById("regEmail").value;
@@ -69,73 +56,70 @@ function register() {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, email, password })
   })
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("registerMsg").innerText = data.message;
-  });
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("registerMsg").innerText = data.message;
+    });
 }
 
-// 📥 FETCH TODOS
+// FETCH TODOS
 function fetchTodos() {
   const token = localStorage.getItem("token");
-  // console.log("token",token);
-
 
   fetch(`${API_URL}/todos`, {
-    headers: { 
+    headers: {
       Authorization: `Bearer ${token}`
-     }
+    }
   })
-  .then(res => res.json())
-  .then(data => {
-    const list = document.getElementById("todoList");
-    list.innerHTML = "";
+    .then(res => res.json())
+    .then(data => {
+      const list = document.getElementById("todoList");
+      list.innerHTML = "";
 
-      // console.log("List",list);
+      if (!Array.isArray(data)) {
+        console.log("Expected array but got ", data);
+        return;
+      }
 
+      data.forEach(todo => {
+        const li = document.createElement("li");
+        li.innerText = todo.title;
 
-    if (!Array.isArray(data)) {
-  console.log("Expected array but got 👉", data);
-  return;
-}
+        li.innerHTML = `
+    <div class="todo-container">
+        <input type="checkbox" ${todo.completed ? 'checked' : ''} onchange="toggleComplete('${todo._id}', this.checked)"></input>
 
-    data.forEach(todo => {
-      const li = document.createElement("li");
-      li.innerText = todo.title;
+        <div class="todo-info">
+            <span class="type-badge">${todo.type}</span> 
+            <strong>${todo.title}</strong>
+            <p>${todo.description || ""}</p>
+        </div>
+    </div>
+`;
 
-      // ✅ TEXT
-      // const text = document.createElement("span");
-      // text.innerText = todo.title ;
-      // span.setAttribute("data-id", todo._id);
+        //  EDIT BUTTON
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.onclick = () => editTodo(todo._id, todo.title, todo.description);
 
-
-    // ✅ EDIT BUTTON
-      const editBtn = document.createElement("button");
-      editBtn.innerText = "Edit";
-      editBtn.onclick = () => editTodo(todo._id, todo.title);
-
-    // ✅ DELETE BUTTON
-      const deleteBtn = document.createElement("button");
-      deleteBtn.innerText = "Delete";
-      deleteBtn.onclick = () => deleteTodo(todo._id);
-
-
-      //li.appendChild(text);
-      li.appendChild(editBtn);
-      li.appendChild(deleteBtn);
-
-    list.appendChild(li);
+        // DELETE BUTTON
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete";
+        deleteBtn.onclick = () => deleteTodo(todo._id);
+        li.appendChild(editBtn);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+      });
     });
-  }); 
 }
 
-
-
-// ➕ ADD TODO
+// ADD TODO
 function addTodo() {
   const token = localStorage.getItem("token");
   const title = document.getElementById("todoInput").value;
-  console.log("title",title);
+  console.log("title", title);
+  const description = document.getElementById("descInput").value;
+  const type = document.querySelector('input[name="todoType"]:checked').value;
 
   fetch(`${API_URL}/todos`, {
     method: "POST",
@@ -143,19 +127,40 @@ function addTodo() {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`
     },
-    body: JSON.stringify({ title })
+    body: JSON.stringify({ title, description, type })
   })
-  .then(() => {
-    document.getElementById("todoInput").value = "";
-    fetchTodos();
-  });
+    .then(() => {
+      document.getElementById("todoInput").value = "";
+      document.getElementById("descInput").value = "";
+      document.querySelector('input[name="todoType"]').value = "";
+      fetchTodos();
+    });
 }
+//Checkbox toggle function
+async function toggleComplete(id, isChecked) {
+  const token = localStorage.getItem("token");
 
+  try {
+    const response = await fetch(`${API_URL}/todos/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+
+      body: JSON.stringify({ completed: isChecked })
+    });
+
+  } catch (error) {
+    console.error("Network error:", error);
+  }
+}
 // EDIT TODO
 
-async function editTodo(id, oldTitle) {
+async function editTodo(id, oldTitle, oldDescription) {
   const token = localStorage.getItem("token");
   const newTitle = prompt("Edit todo", oldTitle);
+  const newDescription = prompt("Edit todo", oldDescription);
 
   if (!newTitle) return;
 
@@ -165,16 +170,17 @@ async function editTodo(id, oldTitle) {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`
     },
-    body: JSON.stringify({ title: newTitle })
+    body: JSON.stringify({
+      title: newTitle,
+      description: newDescription
+    })
   });
-  console.log("before calling  loadtodos");
 
   fetchTodos();
-  // console.log("after calling  loadtodos");
 
-  }
+}
 
-// // DELETE TODO
+// DELETE TODO
 async function deleteTodo(id) {
   const token = localStorage.getItem("token");
 
@@ -186,17 +192,15 @@ async function deleteTodo(id) {
   });
 
   fetchTodos();
-
-   
 }
 
-// 🚪 LOGOUT
+//  LOGOUT
 function logout() {
   localStorage.removeItem("token");
   window.location.href = "login.html";
 }
 
-// 🔒 Protect todo page
+//  Protect todo page
 if (window.location.pathname.includes("todo.html")) {
   if (!localStorage.getItem("token")) {
     window.location.href = "login.html";
